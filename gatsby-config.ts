992dd076037e33527334bfeed5c7e79f076d267f
zivmax <zivmax@foxmail.com>
2024-06-15 05:53:47 +0800

    Solve the prefix problem

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,9 +1,5 @@
 import type { GatsbyConfig } from "gatsby";
 
-module.exports = {
-  pathPrefix: `/shanghaitech-china`,
-};
-
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Shanghaitech iGEM Wiki`,
@@ -13,6 +9,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
+  pathPrefix: `/shanghaitech-china`,
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
