6175d07e471aec83d2ec00e0563206e3ce0fe066
zivmax <zivmax@foxmail.com>
2024-06-15 03:07:51 +0800

    Fix description

--- src/pages/description/index.tsx
+++ src/pages/description/index.tsx
@@ -1,60 +1,45 @@
 import * as React from 'react'
-import { Link, graphql, PageProps } from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
-interface BlogPageProps extends PageProps {
+interface DescriptionPageProps {
   data: {
-    allMdx: {
-      nodes: {
-        id: string
-        frontmatter: {
-          date: string
-          title: string
-          slug: string
-        }
-        excerpt: string
-      }[]
-    }
-  }
+    mdx: {
+      frontmatter: {
+        title: string;
+        date: string;
+      };
+      body: string;
+    };
+  };
 }
 
-const BlogPage: React.FC<BlogPageProps> = ({ data }) => {
+const DescriptionPage: React.FC<DescriptionPageProps> = ({ data }) => {
+  const { frontmatter, body } = data.mdx;
+
   return (
-    <Layout pageTitle="My Blog Posts">
-      {
-        data.allMdx.nodes.map(node => (
-          <article key={node.id}>
-            <h2>
-              <Link to={`/blog/${node.frontmatter.slug}`}>
-                {node.frontmatter.title}
-              </Link>
-            </h2>
-            <p>Posted: {node.frontmatter.date}</p>
-            <p>{node.excerpt}</p>
-          </article>
-        ))
-      }
+    <Layout pageTitle={frontmatter.title}>
+      <p>{frontmatter.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: body }} />
     </Layout>
   )
 }
 
 export const query = graphql`
   query {
-    allMdx(sort: { frontmatter: { date: DESC }}) {
-      nodes {
-        frontmatter {
-          date(formatString: "MMMM D, YYYY")
-          title
-          slug
-        }
-        id
-        excerpt
+    mdx(frontmatter: { type: { eq: "description" } }) {
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
       }
+      body
     }
   }
 `
 
-export const Head: React.FC = () => <Seo title="My Blog Posts" />
+export const Head: React.FC<DescriptionPageProps> = ({ data }) => (
+  <Seo title={data.mdx.frontmatter.title} />
+)
 
-export default BlogPage
+export default DescriptionPage
