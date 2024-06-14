9c84ad225e1971556e656d30318cefedc15612dc
zivmax <zivmax@foxmail.com>
2024-06-15 05:59:08 +0800

    Fix project name

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,11 +5,11 @@ const config: GatsbyConfig = {
     title: `Shanghaitech iGEM Wiki`,
     siteUrl: `https://2024.igem.wiki/shanghaitech-china`,
   },
+  pathPrefix: `/shanghaitech-china`,
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  pathPrefix: `/shanghaitech-china`,
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
