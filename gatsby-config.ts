c131e81f3ed5e2f62daf509fef1770e4a7c57de1
zivmax <zivmax@foxmail.com>
2024-06-19 14:45:58 +0800

    Add back pathPrefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,7 +5,7 @@ const config: GatsbyConfig = {
     title: `Shanghaitech iGEM Wiki`,
     siteUrl: `https://2024.igem.wiki/shanghaitech-china`,
   },
-  pathPrefix: ``,
+  pathPrefix: `/shanghaitech-china`,
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
