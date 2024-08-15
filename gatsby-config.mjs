14bdbdc1d4b8cbbcd6023cc15282b05d9e23c209
zivmax <zivmax@foxmail.com>
2024-08-15 11:09:37 +0800

    optimize: Migrate back to using the purgecss plugin with safelist.

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -101,21 +101,21 @@ const config = {
               escapeEntities: {},
             },
           },
-          // {
-          //   resolve: `gatsby-plugin-purgecss`,
-          //   options: {
-          //     printRejected: true, // Print removed selectors and processed file names
-          //     develop: false, // Enable while using `gatsby develop`
-          //     // tailwind: true, // Enable tailwindcss support
-          //     // ignore: ['navbar.module.scss'], // Ignore files/folders
-          //     // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-          //     purgeCSSOptions: {
-          //       // https://purgecss.com/configuration.html#options
-          //       // safelist: ['safelist'], // Don't remove this selector
-          //     },
-          //     // More options defined here https://purgecss.com/configuration.html#options
-          //   },
-          // },
+          {
+            resolve: `gatsby-plugin-purgecss`,
+            options: {
+              printRejected: true, // Print removed selectors and processed file names
+              develop: true, // Enable while using `gatsby develop`
+              // tailwind: true, // Enable tailwindcss support
+              ignore: ['navbar.module.scss', 'prism.min.css'], // Ignore files/folders
+              // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+              purgeCSSOptions: {
+                // https://purgecss.com/configuration.html#options
+                safelist: ['navbar-module*', 'brand', 'logo'], // Don't remove this selector
+              },
+              // More options defined here https://purgecss.com/configuration.html#options
+            },
+          },
           {
             resolve: `gatsby-remark-katex`,
             options: {
