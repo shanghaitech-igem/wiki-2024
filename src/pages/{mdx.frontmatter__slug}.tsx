381a55b05ea8a5ffbf8a2cd61f1be5bf97874e41
zivmax <zivmax@foxmail.com>
2024-08-27 21:02:50 +0800

    refactor: Abstract out the copy-tex function as a util lib.

--- src/pages/{mdx.frontmatter__slug}.tsx
+++ src/pages/{mdx.frontmatter__slug}.tsx
@@ -14,7 +14,7 @@ import "../styles/katex.scss";
 import "../styles/mdx.global.scss";
 import * as styles from "../styles/mdx.module.scss";
 
-import katexReplaceWithTex from "../utils/katex2tex";
+import handleCopyTex from "../utils/copy-tex";
 
 interface MdxPageProps {
   data: {
@@ -34,67 +34,14 @@ interface MdxPageProps {
 }
 
 const MDXPage: React.FC<MdxPageProps> = ({ data, children }) => {
-  // Return <div class="katex"> element containing node, or null if not found.
-  function closestKatex(node: Node): Element | null | undefined {
-    // If node is a Text Node, for example, go up to containing Element,
-    // where we can apply the `closest` method.
-    const element: Element | null | undefined =
-      node instanceof Element ? node : node.parentElement;
-    return element && element.closest(".katex");
-  }
-
-  const handleCopy = (event: ClipboardEvent) => {
-    console.log("copy event");
-    const selection = window.getSelection();
-    if (!selection || selection.isCollapsed || !event.clipboardData) {
-      return; // default action OK if selection is empty or unchangeable
-    }
-
-    const clipboardData = event.clipboardData;
-    const range = selection.getRangeAt(0); // Now safe to access
-
-    // When start point is within a formula, expand to entire formula.
-    const startKatex = closestKatex(range.startContainer);
-    if (startKatex) {
-      range.setStartBefore(startKatex);
-    }
-
-    // Similarly, when end point is within a formula, expand to entire formula.
-    const endKatex = closestKatex(range.endContainer);
-    if (endKatex) {
-      range.setEndAfter(endKatex);
-    }
-
-    const fragment = range.cloneContents();
-    if (!fragment.querySelector(".katex-mathml")) {
-      return; // default action OK if no .katex-mathml elements
-    }
-
-    const htmlContents = Array.prototype.map
-      .call(fragment.childNodes, (el) =>
-        el instanceof Text ? el.textContent : el.outerHTML
-      )
-      .join("");
-
-    // Preserve usual HTML copy/paste behavior.
-    clipboardData.setData("text/html", htmlContents);
-    // Rewrite plain-text version.
-    console.log(katexReplaceWithTex(fragment).textContent);
-    clipboardData.setData(
-      "text/plain",
-      katexReplaceWithTex(fragment).textContent || ""
-    );
-    // Prevent normal copy handling.
-    event.preventDefault();
-  };
 
   React.useEffect(() => {
     // Attach the copy event listener
-    document.addEventListener("copy", handleCopy);
-
+    document.addEventListener("copy", handleCopyTex);
+  
     // Cleanup event listener on component unmount
     return () => {
-      document.removeEventListener("copy", handleCopy);
+      document.removeEventListener("copy", handleCopyTex);
     };
   }, []);
 
