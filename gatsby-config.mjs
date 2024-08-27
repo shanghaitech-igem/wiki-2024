2b484ebb40900af6134d1fd0ac19c7317aaeac6c
zivmax <zivmax@foxmail.com>
2024-08-27 17:12:07 +0800

    feat: Add copy-tex KaTex extension feature.

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -129,10 +129,10 @@ const config = {
             resolve: `gatsby-plugin-purgecss`,
             options: {
               printRejected: true, // Print removed selectors and processed file names
-              develop: false, // Enable while using `gatsby develop`
+              develop: true, // Enable while using `gatsby develop`
               // tailwind: true, // Enable tailwindcss support
-              ignore: ["navbar.module.scss", "prism.min.css"], // Ignore files/folders
-              // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+              ignore: [], // Ignore files/folders
+              purgeOnly : [], // Purge only these files/folders
               purgeCSSOptions: {
                 // https://purgecss.com/configuration.html#options
                 safelist: [], // Don't remove this selector
@@ -146,7 +146,7 @@ const config = {
               // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
               strict: `ignore`,
               trust: true,
-              output: "mathml",
+              output: "htmlAndMathml",
             },
           },
         ],
