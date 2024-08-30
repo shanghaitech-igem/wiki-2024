350ac6a8eddec01d54977bda9ce6f1680bc1049a
zivmax <zivmax@foxmail.com>
2024-08-30 11:11:25 +0800

    refactor: Split the sytle sheet of MDX page into multiple files.

--- src/pages/{mdx.frontmatter__slug}.tsx
+++ src/pages/{mdx.frontmatter__slug}.tsx
@@ -10,8 +10,11 @@ import Seo from "../components/seo";
 import Favicon from "../components/favicon";
 
 import "../styles/global/prismjs.scss";
-import "../plugins/katex-css/katex.scss";
+import "../styles/global/katex.scss";
+import "../styles/global/footnote.scss";
+import "../styles/global/copy-btn.scss";
 import "../styles/global/mdx.scss";
+
 import * as styles from "../styles/mdx.module.scss";
 
 import handleCopyTex from "../plugins/katex-copy-tex";
