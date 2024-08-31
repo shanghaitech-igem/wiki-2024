a59a64b4b3df46694ef13455ac32c978707a64c5
zivmax <zivmax@foxmail.com>
2024-08-31 15:19:41 +0800

    feat: Use the image viewer for defualt markdown image syntax.

--- src/components/mdx/image.tsx
+++ src/components/mdx/image.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { PhotoProvider, PhotoView } from "../../plugins/image-viewer";
-
 import * as styles from "../../styles/modules/image.module.scss";
 
-interface ImageProps {
-  url: string;
-  alt?: string;
-}
+interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {}
 
-const Image: React.FC<ImageProps> = ({ url, alt }: ImageProps) => {
+const Image: React.FC<ImageProps> = ({ src, alt, ...props }) => {
   return (
     <PhotoProvider className={styles.toppest}>
-      <PhotoView src={url}>
-        <img src={url} alt={alt ? alt : "Failed to load the picture: " + url} />
+      <PhotoView src={src}>
+        <img
+          src={src}
+          alt={alt ? alt : "Failed to load the picture: " + src}
+          {...props}
+        />
       </PhotoView>
     </PhotoProvider>
   );
