8f69ae0975fb7c9c2e0ed815a7db0dcb782d972c
zivmax <zivmax@foxmail.com>
2024-09-04 18:23:49 +0800

    feat: Improve the style of the plot MDX component.

--- src/components/mdx/plot.tsx
+++ src/components/mdx/plot.tsx
@@ -1,16 +1,44 @@
 import React from "react";
 import Loadable from "react-loadable";
+import { PlotParams } from "react-plotly.js";
+import * as styles from "../../styles/modules/plot.module.scss";
 
-const Plot = Loadable({
+// LoadingSpinner Component
+const LoadingSpinner: React.FC = () => {
+  return (
+    <div className={styles.loadingSpinner}>
+      <div className={styles.spinner}></div>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+// TimeoutMessage Component
+const TimeoutMessage: React.FC = () => {
+  return (
+    <div className={styles.timeoutMessage}>
+      <p>Error: Loading Plotly Timed Out.</p>
+    </div>
+  );
+};
+
+// Loadable Plotly Component
+const Plotly = Loadable({
   loader: () => import(`react-plotly.js`),
   loading: ({ timedOut }) =>
-    timedOut ? (
-      <blockquote>Error: Loading Plotly timed out.</blockquote>
-    ) : (
-      <div>Loading... (rendered from the client)</div>
-    ),
-
+    timedOut ? <TimeoutMessage /> : <LoadingSpinner />,
   timeout: 10000,
 });
+
+// Plot Component
+const Plot: React.FC<PlotParams> = (params) => {
+  return (
+    <div className={styles.plotContainer}>
+      <div className={styles.plot}>
+      <Plotly {...params}  useResizeHandler={true} style={{ width: '100%', height: '100%' }} />
+      </div>
+    </div>
+  );
+};
 
 export default Plot;
