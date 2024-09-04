ace04a425e7802f1b227339357c7c59fe27440f8
zivmax <zivmax@foxmail.com>
2024-09-04 20:47:14 +0800

    refactor: Use webpack config to prevent source map being generated.

---
+++ gatsby-node.ts
@@ -0,0 +1,14 @@
+import type { GatsbyNode } from "gatsby";
+
+
+// Disable source maps when building
+export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
+  stage,
+  actions,
+}) => {
+  if (stage === `build-javascript`) {
+    actions.setWebpackConfig({
+      devtool: false,
+    });
+  }
+};
