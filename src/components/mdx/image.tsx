9173207bb74695f852f1e67eafa736d8a5cb4f49
zivmax <zivmax@foxmail.com>
2024-09-17 17:08:24 +0800

    feat: Change the background color of the imge viewer for pure black svgs.

--- src/components/mdx/image.tsx
+++ src/components/mdx/image.tsx
@@ -9,11 +9,12 @@ const staticServerURL = "https://static.igem.wiki/teams/5174/";
 const Image: React.FC<ImageProps> = ({ src, alt, ...props }) => {
   src = src?.startsWith("http") ? src : src?.replace("server/", staticServerURL);
   return (
-    <PhotoProvider className={styles.toppest}>
+    <PhotoProvider className={styles.container}>
       <PhotoView src={src}>
         <img
           src={src}
           alt={alt ? alt : "Failed to load the picture: " + src}
+          className={styles.img}
           {...props}
         />
       </PhotoView>
