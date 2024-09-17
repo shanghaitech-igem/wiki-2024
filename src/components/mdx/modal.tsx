6dfdd967ad24d9bd144002882620e2fb4bde3343
zivmax <zivmax@foxmail.com>
2024-09-17 17:38:28 +0800

    fix: Fix the id passing in of modal, and make the header fixed at the top.

--- src/components/mdx/modal.tsx
+++ src/components/mdx/modal.tsx
@@ -1,31 +1,31 @@
 import * as React from "react";
 import * as styles from "src/styles/modules/modal.module.scss";
 
-const { modalTrigger, modal, overlayer, close, article, header, section } =
-  styles;
+const { modalTrigger, modal, overlayer, close, article, header, section } = styles;
 
 interface ModalProps {
+  id: string;
   title: string;
   triggerText: string;
   children: React.ReactNode;
 }
 
-const Modal: React.FC<ModalProps> = ({ title, triggerText, children }) => {
+const Modal: React.FC<ModalProps> = ({ id, title, triggerText, children }) => {
   return (
     <div>
       {/* Trigger Button */}
-      <label htmlFor="modal_1" className={modalTrigger}>
+      <label htmlFor={id} className={modalTrigger}>
         {triggerText}
       </label>
 
       {/* Modal Structure */}
       <div className={modal}>
-        <input id="modal_1" type="checkbox" />
-        <label htmlFor="modal_1" className={overlayer}></label>
+        <input id={id} type="checkbox" />
+        <label htmlFor={id} className={overlayer}></label>
         <article className={article}>
           <header className={header}>
             <h3>{title}</h3>
-            <label htmlFor="modal_1" className={close}>
+            <label htmlFor={id} className={close}>
               &times;
             </label>
           </header>
