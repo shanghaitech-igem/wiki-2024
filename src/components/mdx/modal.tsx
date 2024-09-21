5bafb1f9dac979a875ce75e8bf456de21cc90adb
zivmax <zivmax@foxmail.com>
2024-09-21 15:05:04 +0800

    feat: Add ability random ID generation if user don't specify it.

--- src/components/mdx/modal.tsx
+++ src/components/mdx/modal.tsx
@@ -1,31 +1,42 @@
 import * as React from "react";
 import * as styles from "src/styles/modules/modal.module.scss";
 
-const { modalTrigger, modal, overlayer, close, article, header, section } = styles;
+const { modalTrigger, modal, overlayer, close, article, header, section } =
+  styles;
 
 interface ModalProps {
-  id: string;
+  id?: string; // Make id optional
   title: string;
   triggerText: string;
   children: React.ReactNode;
 }
 
 const Modal: React.FC<ModalProps> = ({ id, title, triggerText, children }) => {
+  const [generatedId, setGeneratedId] = React.useState<string>("");
+
+  React.useEffect(() => {
+    if (!id) {
+      setGeneratedId(`modal-${Math.random().toString(36).substr(2, 9)}`);
+    }
+  }, [id]);
+
+  const modalId = id || generatedId;
+
   return (
     <div>
       {/* Trigger Button */}
-      <label htmlFor={id} className={modalTrigger}>
+      <label htmlFor={modalId} className={modalTrigger}>
         {triggerText}
       </label>
 
       {/* Modal Structure */}
       <div className={modal}>
-        <input id={id} type="checkbox" />
-        <label htmlFor={id} className={overlayer}></label>
+        <input id={modalId} type="checkbox" />
+        <label htmlFor={modalId} className={overlayer}></label>
         <article className={article}>
           <header className={header}>
             <h3>{title}</h3>
-            <label htmlFor={id} className={close}>
+            <label htmlFor={modalId} className={close}>
               &times;
             </label>
           </header>
