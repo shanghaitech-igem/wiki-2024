b1f8b2a94fb124c9ad56508d37b8742edd39eb55
zivmax <zivmax@foxmail.com>
2024-09-22 20:05:06 +0800

    feat: Add scrollreveal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,49 @@ import Layout from "src/components/layout";
 import Seo from "src/components/seo";
 import Favicon from "src/components/favicon";
 import * as styles from "src/styles/modules/index.module.scss";
+import ScrollReveal from "scrollreveal";
 
-const {title} = styles;
+const { title } = styles;
 
 const IndexPage: React.FC = () => {
+  React.useEffect(() => {
+    if (typeof window !== "undefined") {
+      const options = {
+        distance: "10px",
+      };
+      ScrollReveal().reveal(`.${title}`, options);
+    }
+  }, []);
+
   return (
     <Layout>
-      <header className={title}>Welcome!</header>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
+      <p className={title}>Welcome!</p>
     </Layout>
   );
 };
