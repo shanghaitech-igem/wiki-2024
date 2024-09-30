23b5367f5a3b9dc579cb4533b9f0a7eeb2b7912d
Linzheng Tang <zivmax@foxmail.com>
2024-10-01 00:25:29 +0800

    feat: Add custom cursor.

---
+++ src/components/cursor.tsx
@@ -0,0 +1,31 @@
+// CustomCursor.tsx
+import React from "react";
+import parseRemoteURL from "src/utils/remote-url-parser";
+interface CursorProps {
+  svgURL?: string | null;
+  children?: React.ReactNode;
+}
+
+const Cursor: React.FC<CursorProps> = ({ svgURL, children }) => {
+  if (!svgURL) {
+    return <>{children}</>;
+  }
+  svgURL = parseRemoteURL(svgURL);
+  return (
+    <div
+      ref={(node) => {
+        if (node) {
+          node.style.setProperty(
+            "cursor",
+            `url('${svgURL}') 0 0, pointer`,
+            "important"
+          );
+        }
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Cursor;
