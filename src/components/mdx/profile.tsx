d0dbd4ea41244501ef66aff6f400910c24ac9a95
Linzheng Tang <zivmax@foxmail.com>
2024-10-01 09:27:58 +0800

    feat: Add profile component.

--- src/components/mdx/profile.tsx
+++ src/components/mdx/profile.tsx
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { useSpring, animated } from "@react-spring/web";
 import useMeasure from "react-use-measure";
-
-import "./profile.scss";
+import parseRemoteURL from "src/utils/remote-url-parser";
+import * as styles from "src/styles/modules/profile.module.scss";
 
 interface ProfileProps {
   portrait: string;
   intro: string;
   name: string;
   job: string;
+  reverse?: boolean;
 }
 
-const Profile: React.FC<ProfileProps> = ({ portrait, intro, name, job }) => {
+const Profile: React.FC<ProfileProps> = ({
+  portrait,
+  intro,
+  name,
+  job,
+  reverse = false,
+}) => {
+  portrait = parseRemoteURL(portrait);
   const [isOpen, setIsOpen] = useState(false);
   const [ref, bounds] = useMeasure();
 
@@ -24,8 +32,8 @@ const Profile: React.FC<ProfileProps> = ({ portrait, intro, name, job }) => {
   }, []);
 
   const spring = useSpring({
-    from: { width: windowWidth * 0.2 }, // Start with 20% of the container width
-    to: { width: isOpen ? bounds.width : windowWidth * 0.2 }, // Toggle between 20% and 50%
+    from: { width: windowWidth * 0.2 },
+    to: { width: isOpen ? bounds.width : windowWidth * 0.2 },
   });
 
   const springText = useSpring({
@@ -36,7 +44,11 @@ const Profile: React.FC<ProfileProps> = ({ portrait, intro, name, job }) => {
       height: bounds.height,
     },
     to: {
-      marginLeft: isOpen ? windowWidth * 0.21 : 0,
+      marginLeft: isOpen
+        ? reverse
+          ? windowWidth * 0.05
+          : windowWidth * 0.21
+        : 0,
       width: isOpen ? windowWidth * 0.25 : 0,
       opacity: isOpen ? 1 : 0,
       height: isOpen ? bounds.height : 0,
@@ -44,23 +56,28 @@ const Profile: React.FC<ProfileProps> = ({ portrait, intro, name, job }) => {
   });
 
   return (
-    <div className="primary-container" ref={ref}>
+    <div
+      className={`${styles.primaryContainer} ${
+        reverse ? styles.alignRight : ""
+      }`}
+      ref={ref}
+    >
       <div
         onClick={() => {
           setIsOpen(!isOpen);
         }}
-        className="portrait"
+        className={styles.portrait}
         style={{ backgroundImage: `url(${portrait})` }}
       />
-      <animated.div style={{ ...spring }} className="secondary-container">
-        <animated.div style={{ ...springText }} className="intro">
+      <animated.div style={{ ...spring }} className={styles.secondaryContainer}>
+        <animated.div style={{ ...springText }} className={styles.intro}>
           {intro}
         </animated.div>
       </animated.div>
-      <animated.div style={{ ...spring }} className="third-container">
-        <div className="name-job">
-          <div className="name">{name}</div>
-          <div className="job">{job}</div>
+      <animated.div style={{ ...spring }} className={styles.thirdContainer}>
+        <div className={styles.nameJob}>
+          <div className={styles.name}>{name}</div>
+          <div className={styles.job}>{job}</div>
         </div>
       </animated.div>
     </div>
